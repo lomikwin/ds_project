37092dac477ee4e0289ed988911329a682569da4
lomikwin <133720777+lomikwin@users.noreply.github.com>
2026-08-27 23:22:16 +0900

    Get from Webpage

--- station_price/station_day.py
+++ station_price/station_day.py
@@ -15,5 +15,10 @@ nfl_url = ("https://nfl.opinet.co.kr/ts.wseq"
            "&sid=service_1&aid=B7&js=yes")
 
 r2 = s.get(nfl_url, timeout=15)
-r2_url = r2.text.split('result=')[1].split("'")[1]
-print(r2_url)
+r2_chunk = r2.text.split('result=')[1].split("'")[1].split(":",2)
+status_code =r2_chunk[0]
+waiting_code =r2_chunk[1]
+query_string =r2_chunk[2]
+print("status_code:", status_code)
+print("waiting_code:", waiting_code)
+print("query_string:", query_string)
