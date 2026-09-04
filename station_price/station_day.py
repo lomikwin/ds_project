dea7b3444b8f1d731086d877a56f912722cb57d5
lomikwin <133720777+lomikwin@users.noreply.github.com>
2026-09-04 22:38:32 +0900

    Get from Webpage - 20260904-1

--- station_price/station_day.py
+++ station_price/station_day.py
@@ -1,5 +1,5 @@
 import requests
-import urllib
+from urllib.parse import parse_qs , parse_qsl
 
 s = requests.Session()
 url = "https://www.opinet.co.kr/user/opdown/opDownload.do"
@@ -15,10 +15,8 @@ nfl_url = ("https://nfl.opinet.co.kr/ts.wseq"
            "&sid=service_1&aid=B7&js=yes")
 
 r2 = s.get(nfl_url, timeout=15)
-r2_chunk = r2.text.split('result=')[1].split("'")[1].split(":",2)
-status_code =r2_chunk[0]
-waiting_code =r2_chunk[1]
-query_string =r2_chunk[2]
-print("status_code:", status_code)
-print("waiting_code:", waiting_code)
-print("query_string:", query_string)
+_, status_code ,  query_string = r2_chunk = r2.text.split('result=')[1].split("'")[1].split(":",2)
+
+parsed = parse_qs(query_string)
+
+print(parsed)
