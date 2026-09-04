bfa99d4ac1bd04e49f7c628256016d397e76c367
lomikwin <133720777+lomikwin@users.noreply.github.com>
2026-09-04 22:57:35 +0900

    Get from Webpage - 20260904-2

--- station_price/station_day.py
+++ station_price/station_day.py
@@ -17,6 +17,28 @@ nfl_url = ("https://nfl.opinet.co.kr/ts.wseq"
 r2 = s.get(nfl_url, timeout=15)
 _, status_code ,  query_string = r2_chunk = r2.text.split('result=')[1].split("'")[1].split(":",2)
 
-parsed = parse_qs(query_string)
+netfunnel_key = parse_qs(query_string)['key'][0]
 
-print(parsed)
+download_url = "https://www.opinet.co.kr/user/main/main_download_csv_big.do"
+
+target_dt = "20260903"
+dl_payload = {
+    "rdo1":"A", "rdo2":"A" , "rdo3":"A", "rdo4":"X",
+    "LPG_CD":"A",
+    "DATE_DIV_CD":"X",
+    "PAGE_DIV":"PAGE_DIV_6",
+    "SIDO_NM": "시/도",
+    "SIGUN_NM":"시/군/구",
+    "API_GBN":"A",
+    "START_DT":target_dt,
+    "END_DT":target_dt,
+    "SIDO_CD":"",
+    "SIGUN_CD":"",
+    "netfunnel_key":netfunnel_key
+}
+
+r3 = s.post(dl_url, data=dl_payload, timeout=15)
+print("len:", len(r3.content))
+
+with open("/volume2/ds_project/station_price", "wb") as f:      
+    f.write(r3.content)
