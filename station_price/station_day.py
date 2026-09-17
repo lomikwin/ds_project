df3ef048388b5ece3d366f9e4f74d1989e78682d
lomikwin <133720777+lomikwin@users.noreply.github.com>
2026-09-17 23:27:42 +0900

    Get from Webpage - 20260917-8

--- station_price/station_day.py
+++ station_price/station_day.py
@@ -63,7 +63,7 @@ df = pd.read_csv ( io.BytesIO(r3.content), encoding = 'cp949', skiprows = [1])
 sql_df = con.sql("""
         select  
         t1.번호 as uni_cd ,
-        date_parse(t1.기간 ,  '%Y%m%d') as part_dt,
+        strftime(t1.기간 ,  '%Y%m%d') as part_dt,
         t2.area_cd , 
         t1.지역 as area_nm
         from df t1 
